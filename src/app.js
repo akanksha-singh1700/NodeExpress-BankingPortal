7841cb02f75aa9f6492400b9380bb57cea64ac74
a_singh <65903559+akanksha-singh1700@users.noreply.github.com>
2023-01-28 21:23:17 +0530

    Module2

--- src/app.js
+++ src/app.js
@@ -1,12 +1,32 @@
-const fs= require('fs');
-const path=require('path');
-const express=require('express');
-const app=express();
-app.set('views', path.join(__dirname,'views'));
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname,'public')));
-app.get('/', function(req, res){
-    res.render('index', {title: 'Index'});
+app.use(express.static(path.join(__dirname, 'public')));
+
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'utf8' });
+const users = JSON.parse(userData);
+
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Account Summary', accounts: accounts });
 });
-app.listen(3000, function(){
-console.log('PS Project Running on port 3000!');});
+app.get('/savings', function (req, res) {
+    res.render('account', { account: accounts.savings });
+});
+app.get('/checking', function (req, res) {
+    res.render('account', { account: accounts.checking });
+});
+app.get('/credit', function (req, res) {
+    res.render('account', { account: accounts.credit });
+});
+app.get('/profile', function(req, res){
+res.render('profile',{user: users[0]});
+});
+app.listen(3000, function () {
+    console.log('PS Project Running on port 3000!');
+});
